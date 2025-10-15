076a335cb888448ccdf903f4f07cf0da02bb15a5
Jiwon Kim <115946088+JiwonKim-kr@users.noreply.github.com>
2025-10-15 15:14:01 +0900

    Title: 등급 매기기, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251015/등급 매기기/ranking.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(' ');
+let score = parseInt(input);
+
+let grade = ["A", "B", "C", "D", "F"];
+
+for(let i = 0; i<5; i++) {
+    if(score >= (90 - i*10)) {
+        console.log(grade[i]);
+        break;
+    }
+}
+if(score < 60) console.log(grade[4]);
