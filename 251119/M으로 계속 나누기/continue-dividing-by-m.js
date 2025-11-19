2cace4941cca6c0cd3f8fd80c1c1b4e791733814
Jiwon Kim <115946088+JiwonKim-kr@users.noreply.github.com>
2025-11-19 14:58:27 +0900

    Title: M으로 계속 나누기, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251119/M으로 계속 나누기/continue-dividing-by-m.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let a = fs.readFileSync(0).toString();
+let arr = a.split(" ");
+
+let n = Number(arr[0]);
+let m = Number(arr[1]);
+
+// Please write your code here.
+
+while (n >= 1) {
+    console.log(Math.floor(n));
+    n /= m;
+}
